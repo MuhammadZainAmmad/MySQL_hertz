b91169fe7be0cfb09e17484a6b4b1c87111cc963
MuhammadZainAmmad <zainammad05@gmail.com>
2023-07-11 07:29:59 -0700

    integrated google sheets code in main.py

--- task1_heirloom_20ListingProj/main.py
+++ task1_heirloom_20ListingProj/main.py
@@ -14,6 +14,15 @@ from email.mime.text import MIMEText
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
 
+# For Google Sheets
+import gspread
+from gspread_dataframe import set_with_dataframe
+
+from google.oauth2.service_account import Credentials
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+
 # # For airflow sheduling 
 # from datetime import timedelta, datetime
 # from airflow import DAG
@@ -31,102 +40,102 @@ conn = mysql.connector.connect(
 cursor = conn.cursor()
 
 sql_queries = [
-        # """
-        #     SELECT 
-        #         COUNT(*) AS TotalRecords
-        #     FROM 20_Listing_Proj;
-        # """,
-        # """
-        #     SELECT
-        #         'Annual Projection' AS Column_Name,
-        #         MIN(`Annual Projection`) AS MinVal,
-        #         MAX(`Annual Projection`) AS MaxVal,
-        #         AVG(`Annual Projection`) AS AvgVal,
-        #         SUM(`Annual Projection`) AS SumVal
-        #     FROM 20_Listing_Proj
-        #     UNION
-        #     SELECT
-        #         'Market_Monthly_Proj',
-        #         MIN(Market_Monthly_Proj),
-        #         MAX(Market_Monthly_Proj),
-        #         AVG(Market_Monthly_Proj),
-        #         SUM(Market_Monthly_Proj)
-        #     FROM 20_Listing_Proj
-        #     UNION
-        #     SELECT
-        #         'Projected Rev %',
-        #         MIN(`Projected Rev %`),
-        #         MAX(`Projected Rev %`),
-        #         AVG(`Projected Rev %`),
-        #         SUM(`Projected Rev %`)
-        #     FROM 20_Listing_Proj
-        #     UNION
-        #     SELECT
-        #         'Projected Occupancy',
-        #         MIN(`Projected Occupancy`),
-        #         MAX(`Projected Occupancy`),
-        #         AVG(`Projected Occupancy`),
-        #         SUM(`Projected Occupancy`)
-        #     FROM 20_Listing_Proj
-        #     UNION
-        #     SELECT
-        #         'Monthly Proj',
-        #         MIN(`Monthly Proj`),
-        #         MAX(`Monthly Proj`),
-        #         AVG(`Monthly Proj`),
-        #         SUM(`Monthly Proj`)
-        #     FROM 20_Listing_Proj
-        #     UNION
-        #     SELECT
-        #         'Final Monthly Projection',
-        #         MIN(`Final Monthly Projection`),
-        #         MAX(`Final Monthly Projection`),
-        #         AVG(`Final Monthly Projection`),
-        #         SUM(`Final Monthly Projection`)
-        #     FROM 20_Listing_Proj
-        #     UNION
-        #     SELECT
-        #         'Market_Monthly_Proj_New',
-        #         MIN(Market_Monthly_Proj_New),
-        #         MAX(Market_Monthly_Proj_New),
-        #         AVG(Market_Monthly_Proj_New),
-        #         SUM(Market_Monthly_Proj_New)
-        #     FROM 20_Listing_Proj;
-        # """,
-        # """
-        #     SELECT 
-        #         DISTINCT Market,
-        #         MIN(`Annual Projection`) AS Min_AnnualProjection,
-        #         MAX(`Annual Projection`) AS Max_AnnualProjection,
-        #         AVG(`Annual Projection`) AS Avg_AnnualProjection,
-        #         SUM(`Annual Projection`) AS Total_AnnualProjection,
-        #         MIN(`Market_Monthly_Proj`) AS Min_MarketMonthlyProj,
-        #         MAX(`Market_Monthly_Proj`) AS Max_MarketMonthlyProj,
-        #         AVG(`Market_Monthly_Proj`) AS Avg_MarketMonthlyProj,
-        #         SUM(`Market_Monthly_Proj`) AS Total_MarketMonthlyProj,
-        #         MIN(`Projected Rev %`) AS Min_ProjectedRevPercent,
-        #         MAX(`Projected Rev %`) AS Max_ProjectedRevPercent,
-        #         AVG(`Projected Rev %`) AS Avg_ProjectedRevPercent,
-        #         SUM(`Projected Rev %`) AS Total_ProjectedRevPercent,
-        #         MIN(`Projected Occupancy`) AS Min_ProjectedOccupancy,
-        #         MAX(`Projected Occupancy`) AS Max_ProjectedOccupancy,
-        #         AVG(`Projected Occupancy`) AS Avg_ProjectedOccupancy,
-        #         SUM(`Projected Occupancy`) AS Total_ProjectedOccupancy,
-        #         MIN(`Monthly Proj`) AS Min_MonthlyProj,
-        #         MAX(`Monthly Proj`) AS Max_MonthlyProj,
-        #         AVG(`Monthly Proj`) AS Avg_MonthlyProj,
-        #         SUM(`Monthly Proj`) AS Total_MonthlyProj,
-        #         MIN(`Final Monthly Projection`) AS Min_FinalMonthlyProj,
-        #         MAX(`Final Monthly Projection`) AS Max_FinalMonthlyProj,
-        #         AVG(`Final Monthly Projection`) AS Avg_FinalMonthlyProj,
-        #         SUM(`Final Monthly Projection`) AS Total_FinalMonthlyProj,
-        #         MIN(`Market_Monthly_Proj_New`) AS Min_MarketMonthlyProjNew,
-        #         MAX(`Market_Monthly_Proj_New`) AS Max_MarketMonthlyProjNew,
-        #         AVG(`Market_Monthly_Proj_New`) AS Avg_MarketMonthlyProjNew,
-        #         SUM(`Market_Monthly_Proj_New`) AS Total_MarketMonthlyProjNew
-        #     FROM 20_Listing_Proj
-        #     GROUP BY Market;
-        # """,
+        """
+            SELECT 
+                COUNT(*) AS TotalRecords
+            FROM 20_Listing_Proj;
+        """,
+        """
+            SELECT
+                'Annual Projection' AS Column_Name,
+                MIN(`Annual Projection`) AS MinVal,
+                MAX(`Annual Projection`) AS MaxVal,
+                AVG(`Annual Projection`) AS AvgVal,
+                SUM(`Annual Projection`) AS SumVal
+            FROM 20_Listing_Proj
+            UNION
+            SELECT
+                'Market_Monthly_Proj',
+                MIN(Market_Monthly_Proj),
+                MAX(Market_Monthly_Proj),
+                AVG(Market_Monthly_Proj),
+                SUM(Market_Monthly_Proj)
+            FROM 20_Listing_Proj
+            UNION
+            SELECT
+                'Projected Rev %',
+                MIN(`Projected Rev %`),
+                MAX(`Projected Rev %`),
+                AVG(`Projected Rev %`),
+                SUM(`Projected Rev %`)
+            FROM 20_Listing_Proj
+            UNION
+            SELECT
+                'Projected Occupancy',
+                MIN(`Projected Occupancy`),
+                MAX(`Projected Occupancy`),
+                AVG(`Projected Occupancy`),
+                SUM(`Projected Occupancy`)
+            FROM 20_Listing_Proj
+            UNION
+            SELECT
+                'Monthly Proj',
+                MIN(`Monthly Proj`),
+                MAX(`Monthly Proj`),
+                AVG(`Monthly Proj`),
+                SUM(`Monthly Proj`)
+            FROM 20_Listing_Proj
+            UNION
+            SELECT
+                'Final Monthly Projection',
+                MIN(`Final Monthly Projection`),
+                MAX(`Final Monthly Projection`),
+                AVG(`Final Monthly Projection`),
+                SUM(`Final Monthly Projection`)
+            FROM 20_Listing_Proj
+            UNION
+            SELECT
+                'Market_Monthly_Proj_New',
+                MIN(Market_Monthly_Proj_New),
+                MAX(Market_Monthly_Proj_New),
+                AVG(Market_Monthly_Proj_New),
+                SUM(Market_Monthly_Proj_New)
+            FROM 20_Listing_Proj;
+        """,
+        """
+            SELECT 
+                DISTINCT Market,
+                MIN(`Annual Projection`) AS Min_AnnualProjection,
+                MAX(`Annual Projection`) AS Max_AnnualProjection,
+                AVG(`Annual Projection`) AS Avg_AnnualProjection,
+                SUM(`Annual Projection`) AS Total_AnnualProjection,
+                MIN(`Market_Monthly_Proj`) AS Min_MarketMonthlyProj,
+                MAX(`Market_Monthly_Proj`) AS Max_MarketMonthlyProj,
+                AVG(`Market_Monthly_Proj`) AS Avg_MarketMonthlyProj,
+                SUM(`Market_Monthly_Proj`) AS Total_MarketMonthlyProj,
+                MIN(`Projected Rev %`) AS Min_ProjectedRevPercent,
+                MAX(`Projected Rev %`) AS Max_ProjectedRevPercent,
+                AVG(`Projected Rev %`) AS Avg_ProjectedRevPercent,
+                SUM(`Projected Rev %`) AS Total_ProjectedRevPercent,
+                MIN(`Projected Occupancy`) AS Min_ProjectedOccupancy,
+                MAX(`Projected Occupancy`) AS Max_ProjectedOccupancy,
+                AVG(`Projected Occupancy`) AS Avg_ProjectedOccupancy,
+                SUM(`Projected Occupancy`) AS Total_ProjectedOccupancy,
+                MIN(`Monthly Proj`) AS Min_MonthlyProj,
+                MAX(`Monthly Proj`) AS Max_MonthlyProj,
+                AVG(`Monthly Proj`) AS Avg_MonthlyProj,
+                SUM(`Monthly Proj`) AS Total_MonthlyProj,
+                MIN(`Final Monthly Projection`) AS Min_FinalMonthlyProj,
+                MAX(`Final Monthly Projection`) AS Max_FinalMonthlyProj,
+                AVG(`Final Monthly Projection`) AS Avg_FinalMonthlyProj,
+                SUM(`Final Monthly Projection`) AS Total_FinalMonthlyProj,
+                MIN(`Market_Monthly_Proj_New`) AS Min_MarketMonthlyProjNew,
+                MAX(`Market_Monthly_Proj_New`) AS Max_MarketMonthlyProjNew,
+                AVG(`Market_Monthly_Proj_New`) AS Avg_MarketMonthlyProjNew,
+                SUM(`Market_Monthly_Proj_New`) AS Total_MarketMonthlyProjNew
+            FROM 20_Listing_Proj
+            GROUP BY Market;
+        """,
         """
             SELECT 
                 SUM(`Final Monthly Projection`) AS Total_FinalMonthlyProj,
@@ -157,7 +166,7 @@ sql_queries = [
         """
 ]
 
-# For testing
+# For testing the connection
 # sql_queries = [
 #     """
 #             SELECT 
@@ -166,12 +175,49 @@ sql_queries = [
 #         """
 # ]
 
-# setup for slack
+# ***************************** Querying db, getting reult set in df, saving all response set in a list ********************
+pd.options.display.float_format = '{:,.4f}'.format # for dealing with large float values in resultset dfs
+list_responseSets = []
+for query in sql_queries: 
+    responseSet_df = pd.read_sql(query, conn)
+    list_responseSets.append(responseSet_df) 
+
+cursor.close()
+conn.close()
+
+# **************************** setup for slack *********************************
 channel_id = "C05E96Q6PFB"
 slack_token = os.environ['slackAPIToken_task1']
 client = WebClient(token=slack_token)
 
-# setup for gmail
+# ********* sending each response set in a different message on slack ********************* 
+
+# cant send more than 5 columns in one message on slack with proper formatting  
+# cant send more than 35 rows in one message on slack with proper formatting 
+for res in list_responseSets:   
+    if len(res.columns)>5: # if result set has more than 5 columns (query3), split df on column axis
+        df_market = res.iloc[:, 0] # separating first column (market)
+        for i in range(1, len(res.columns), 4):
+            df = pd.concat([df_market, res.iloc[:, i:i+4]], axis=1)
+            if len(df) > 35:
+                for firstRow in range(0, len(df)-29, 30):
+                    splitRes = df.iloc[firstRow:firstRow+30, : ]
+                    markdown_table = splitRes.to_markdown(index=False, tablefmt="grid", floatfmt=".4f")
+                    response = client.chat_postMessage(channel=channel_id, text= f"```{markdown_table}```")
+            else:
+                markdown_table = df.to_markdown(index=False, tablefmt="grid", floatfmt=".4f")
+                response = client.chat_postMessage(channel=channel_id, text= f"```{markdown_table}```")
+    else:
+        if len(res) > 35:
+            for firstRow in range(0, len(res)-29, 30):
+                splitRes = res.iloc[firstRow:firstRow+30, : ]
+                markdown_table = splitRes.to_markdown(index=False, tablefmt="grid", floatfmt=".4f")
+                response = client.chat_postMessage(channel=channel_id, text= f"```{markdown_table}```")
+        else:
+            markdown_table = res.to_markdown(index=False, tablefmt="grid", floatfmt=".4f")
+            response = client.chat_postMessage(channel=channel_id, text= f"```{markdown_table}```")
+
+# ***************************** setup for gmail **********************************
 def service_account_login():
     SCOPES = ['https://www.googleapis.com/auth/gmail.send']
     client_credentials = service_account.Credentials.from_service_account_info({
@@ -194,40 +240,7 @@ def service_account_login():
     return service
 service=service_account_login()
 
-# Querying db, getting reult set in df, saving all response set in a list
-pd.options.display.float_format = '{:,.2f}'.format # for dealing with large float values in resultset dfs
-list_responseSets = []
-for query in sql_queries: 
-    responseSet_df = pd.read_sql(query, conn)
-    list_responseSets.append(responseSet_df)  
-
-# sending each response set in a different message on slack  
-# cant send more than 5 columns in one message on slack with proper formatting  
-# cant send more than 35 rows in one message on slack with proper formatting 
-for res in list_responseSets:   
-    if len(res.columns)>5: # if result set has more than 5 columns (query3), split df on column axis
-        df_market = res.iloc[:, 0] # separating first column (market)
-        for i in range(1, len(res.columns), 4):
-            df = pd.concat([df_market, res.iloc[:, i:i+4]], axis=1)
-            if len(df) > 35:
-                for firstRow in range(0, len(df)-29, 30):
-                    splitRes = df.iloc[firstRow:firstRow+30, : ]
-                    markdown_table = splitRes.to_markdown(index=False, tablefmt="grid")
-                    response = client.chat_postMessage(channel=channel_id, text= f"```{markdown_table}```")
-            else:
-                markdown_table = df.to_markdown(index=False, tablefmt="grid")
-                response = client.chat_postMessage(channel=channel_id, text= f"```{markdown_table}```")
-    else:
-        if len(res) > 35:
-            for firstRow in range(0, len(res)-29, 30):
-                splitRes = res.iloc[firstRow:firstRow+30, : ]
-                markdown_table = splitRes.to_markdown(index=False, tablefmt="grid")
-                response = client.chat_postMessage(channel=channel_id, text= f"```{markdown_table}```")
-        else:
-            markdown_table = res.to_markdown(index=False, tablefmt="grid")
-            response = client.chat_postMessage(channel=channel_id, text= markdown_table)
-        
-# sending all response set as one email on gmail account
+# ******************************** sending all response set as one email on gmail account ***************************
 mergeHTML = ''
 for res in list_responseSets:
     html = res.to_html(index=False, justify = 'left', border = 2)
@@ -238,9 +251,27 @@ message['from'] = "it@stayloom.com"
 message['subject'] = "Test"
 message = (service.users().messages().send(userId="me", body={'raw': base64.urlsafe_b64encode(message.as_string().encode()).decode()}).execute())
 
+# ********************************* setup for Google Sheets *****************************
+scopes = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
+credentials = Credentials.from_service_account_file('./credentials.json', scopes=scopes)
 
-cursor.close()
-conn.close()
+gc = gspread.authorize(credentials)
+
+gauth = GoogleAuth()
+drive = GoogleDrive(gauth)
+
+gs = gc.open_by_key('1FwaXhwL4jF3a3AstjOsrzCyEmaCA8G2POEuSWSucCcg') # open a google sheet
+
+# ********************************* Filling Google Sheets with result sets *****************************
+for resIndx in range(len(list_responseSets)):  
+    df = list_responseSets[resIndx] 
+    sheetTitle = f'Sheet{resIndx+1}'
+    # if (gs.worksheet(sheetTitle)):
+    #     gs.del_worksheet(sheetTitle)
+    worksheet = gs.worksheet(sheetTitle)
+    worksheet.clear()
+    set_with_dataframe(worksheet=worksheet, dataframe=df, include_index=False, include_column_header=True, resize=True) 
+
 
 # myFunc()
 
